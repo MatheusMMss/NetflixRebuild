716c5251949a293813d20efbeaa6872ae7154d55
matheus martins <matheus_11martins@outlook.com>
2022-06-04 15:02:07 -0300

    Start commit!

---
+++ app/src/main/java/com/netflixrebuild/MovieActivity.java
@@ -0,0 +1,24 @@
+package com.netflixrebuild;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class MovieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+            getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24);
+        }
+    }
+}
---
+++ app/src/main/java/com/netflixrebuild/model/Movie.java
@@ -0,0 +1,14 @@
+package com.netflixrebuild.model;
+
+public class Movie {
+
+    private int coverUrl;
+
+    public int getCoverUrl() {
+        return coverUrl;
+    }
+
+    public void setCoverUrl(int coverUrl) {
+        this.coverUrl = coverUrl;
+    }
+}
